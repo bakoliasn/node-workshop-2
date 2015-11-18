a321961676e3c21072599f106a066feee894fea2
bakoliasn <bakoliasn@gmail.com>
2015-11-18 19:48:46 +0000

    exposed fortune function!

---
+++ library/fortune.js
@@ -0,0 +1,4 @@
+var fortunes = ["you will die", "i will kill you", "we will die", "we will be killed", "everyone will die"];
+var f = fortunes[Math.floor(Math.random()*fortunes.length)];
+console.log(f);
+//function fortune
